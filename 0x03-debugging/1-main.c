8912667cbb665c6dcfe3d63c4f2c7609b856f5d2
KopanoNompumelelo <kopanoradebe888@gmail.com>
2023-03-03 12:26:28 -0800

    1-main.c:14: WARNING: Missing a blank line after declarations

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 	int i;
 	printf("infinite loop incoming :(\n");
 	i = 0;
-	/*while (i < 10); */
+	/*while (i < 10);*/
 	/*{*/
-	/*       putchar(i);*/
+	/*      putchar(i);*/
 	/*}*/
 	printf("infinite loo avoided! \\o/\n");
 	return (0);
