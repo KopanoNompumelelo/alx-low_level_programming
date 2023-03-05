512f32cb5b7148888ac80434f0620414fc41a88a
KopanoNompumelelo <kopanoradebe888@gmail.com>
2023-03-05 08:10:38 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
- /**
-  * _isupper - checkes upper case
-  * @c: print int
-  * Return: give 1 or 0
-  */
+/**
+ * _isupper - cheches upper case
+ * @c: print int
+ * Return: give 1 or 0
+ */
 
 int _isupper(int c)
-
 {
-
 	if (c >= 'A' && c <= 'Z')
 	{
 	return (1);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,11 @@
-int _putchar(char c);
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
-inut mul(int a, int b);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -10,3 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
