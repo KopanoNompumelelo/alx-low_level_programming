d633930ae77672b8d2c145a3975fa7be73ef1bd1
KopanoNompumelelo <kopanoradebe888@gmail.com>
2023-03-14 03:54:36 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * create_array - array for prints a string
+ * @size: number elements array
+ * @c: char
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, cahr c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+
+	/*Define values with malloc*/
+	buffer = (char *) malloc(size * sizeof(0));
+
+	if (buffer == 0)
+	{
+	return (NULL);
+	}
+
+	else
+	{
+	postion = 0;
+	while (postion < size) /*while for array*/
+	{
+	*(buffer + position = c;
+	postion++;
+	}
+	return (buffer);
+	}
+}
